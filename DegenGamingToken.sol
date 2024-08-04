560d48c9b34eb12fdc76da449123cda044dd5a01
tobiasrahul <115544502+tobiasrahul@users.noreply.github.com>
2024-08-04 10:22:35 +0530

    Update DegenGamingToken.sol

--- DegenGamingToken.sol
+++ DegenGamingToken.sol
@@ -6,40 +6,53 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenGamingToken is ERC20, Ownable {
 
-    event TokensRedeemed(address indexed user, uint amount, string itemName);
+    event TokensRedeemed(address indexed user, uint256 amount, string itemName);
 
-
+    // Mapping to store item prices
     mapping(string => uint256) public itemPrices;
 
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {}
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        
+        itemPrices["Minecraft"] = 100;                      //Some item already mapped with thier amount
+        itemPrices["BGMI"] = 70;
+        itemPrices["Free Fire"] = 50;
+        itemPrices["Clash of Clans"] = 50;
+        itemPrices["Zombie Game"] = 40;
+        itemPrices["Shooting"] = 30;
+    }
 
+    
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
+    
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 
-    function transferToken(address _toAddress, uint _amount) public {
-        bool success = transfer(_toAddress, _amount);
-        assert(success);  
+    
+    function transferToken(address to, uint256 amount) public {
+        transfer(to, amount);
     }
 
- 
+   
     function setItemPrice(string memory itemName, uint256 price) public onlyOwner {
         itemPrices[itemName] = price;
     }
 
+    // Function to redeem tokens for an item
     function redeemToken(string memory itemName) public {
         uint256 amount = itemPrices[itemName];
         require(amount > 0, "Item does not exist");
         require(balanceOf(msg.sender) >= amount, "Insufficient token balance to redeem");
         _burn(msg.sender, amount);
+        
+        
         emit TokensRedeemed(msg.sender, amount, itemName);
     }
 
-    function checkTokenBalance(address _account) public view returns (uint) {
-        return balanceOf(_account);
+    function checkTokenBalance(address account) public view returns (uint256) {
+        return balanceOf(account);
     }
 }
